02ddae7a49d7bb667278f4dc989082294b41244d
Flax163 <goncharov.valia@yandex.ru>
2016-08-03 23:37:12 +0400

    refactoring

---
+++ core/src/main/java/org/vago/onlinestore/service/impl/LoadingCategoryServiceImpl.java
@@ -0,0 +1,32 @@
+package org.vago.onlinestore.service.impl;
+
+import org.vago.onlinestore.ConstantsQuery;
+import org.vago.onlinestore.model.CategoryOffer;
+import org.vago.onlinestore.service.LoadingCategoryService;
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.math.BigInteger;
+import java.util.List;
+
+@Stateless
+public class LoadingCategoryServiceImpl implements LoadingCategoryService
+{
+    @PersistenceContext(unitName = "online-store-persistence-unit")
+    EntityManager entityManager;
+
+    @Override
+    public List<CategoryOffer> loadAllCategories()
+    {
+        return entityManager
+                .createQuery(ConstantsQuery.SELECT_ALL_CATEGORY_QUERY, CategoryOffer.class)
+                .getResultList();
+    }
+
+    @Override
+    public CategoryOffer loadCategory(BigInteger id)
+    {
+        return entityManager.find(CategoryOffer.class, id);
+    }
+}
---
+++ core/src/main/java/org/vago/onlinestore/service/impl/LoadingOfferServiceImpl.java
@@ -0,0 +1,25 @@
+package org.vago.onlinestore.service.impl;
+
+import org.vago.onlinestore.model.Offer;
+import org.vago.onlinestore.service.LoadingOfferService;
+
+import javax.ejb.Stateless;
+import javax.ejb.TransactionAttribute;
+import javax.ejb.TransactionAttributeType;
+import javax.persistence.*;
+import java.math.BigInteger;
+import java.util.Collection;
+import java.util.List;
+
+@Stateless
+public class LoadingOfferServiceImpl implements LoadingOfferService
+{
+    @PersistenceContext(unitName = "online-store-persistence-unit")
+    EntityManager entityManager;
+ 
+    @Override
+    public Offer loadOffer(BigInteger id)
+    {
+        return entityManager.find(Offer.class, id);
+    }
+}
