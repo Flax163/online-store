384c4237a8256f89097b35a7f9c9f51cb34aee4f
Flax163 <goncharov.valia@yandex.ru>
2016-08-30 17:01:38 +0400

    config get response

--- web/src/main/frontend/server.js
+++ web/src/main/frontend/server.js
@@ -5,14 +5,22 @@ var app = express();
 
 app.use(express.static('dist'));
 
-app.get('/test', function(req, res) {
-    console.log('Test');
+app.get('/catalogs', function (req, res) {
+    var catalogs = [{
+        id: 1,
+        name: 'category 1'
+    },
+    {
+        id: 2,
+        name: 'category 2'
+    }];
+    res.json(catalogs);
 });
 
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
     res.sendfile('index.html');
 });
 
-app.listen(3000, function() {
+app.listen(3000, function () {
     console.log('Server Start');
 });
