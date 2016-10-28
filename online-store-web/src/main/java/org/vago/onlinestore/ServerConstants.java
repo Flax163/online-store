6322a9737f12558ce8e2f2777c93cf88f1b07c5e
Flax163 <goncharov.valia@yandex.ru>
2016-10-29 02:14:56 +0400

    * fix for start wildfly

--- online-store-web/src/main/java/org/vago/onlinestore/ServerConstants.java
+++ online-store-web/src/main/java/org/vago/onlinestore/ServerConstants.java
@@ -2,7 +2,7 @@ package org.vago.onlinestore;
 
 public final class ServerConstants
 {
-    public static final String ONLINE_STORE_PATH_APPLICATION = "/";
+    public static final String ONLINE_STORE_PATH_APPLICATION = "/rest";
     public static final String ONLINE_STORE_PATH_END_POINT = "/jrs";
 
     public static final String ONLINE_STORE_PATH_LOAD_OFFER = "/category/{category}/offer/{offer}";
