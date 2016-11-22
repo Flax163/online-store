da704ac48c1e684b9e571f0c33fc6a0fdbf38c14
Flax163 <goncharov.valia@yandex.ru>
2016-11-22 21:06:02 +0400

    * add filter authorization

--- authorization-module/authorization-module-impl/src/main/java/ru/vago/authorization/service/filter/AuthorizationRequestFilter.java
+++ authorization-module/authorization-module-impl/src/main/java/ru/vago/authorization/service/filter/AuthorizationRequestFilter.java
@@ -10,9 +10,11 @@ import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.container.ContainerRequestFilter;
 import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.Provider;
 import java.io.IOException;
 
 @Secured
+@Provider
 public class AuthorizationRequestFilter implements ContainerRequestFilter
 {
     private final AuthorizationService authorizationService;
--- online-store-web/src/main/java/org/vago/onlinestore/config/OnlineStoreApplication.java
+++ online-store-web/src/main/java/org/vago/onlinestore/config/OnlineStoreApplication.java
@@ -3,6 +3,7 @@ package org.vago.onlinestore.config;
 import com.google.common.collect.Sets;
 import org.vago.onlinestore.endpoint.AuthorizationEndPointImpl;
 import org.vago.onlinestore.endpoint.OfferServiceEndPointImpl;
+import ru.vago.authorization.service.filter.AuthorizationRequestFilter;
 
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
